cdb170dfe1e513d149b0da0c761829ba4fc9f8ec
Tom Benbow <tom@hellomoon.net>
2022-11-17 14:27:56 -0600

    Simple versxion of footer revs

--- components/layout.tsx
+++ components/layout.tsx
@@ -156,6 +156,14 @@ export const Layout = ({
           <div className="flex flex-col flex-1">{children}</div>
           {/* Footer Blocks */}
           <Blocks { ...globalData } />
+          <div className="bg-primary text-white px-10 py-6">
+            <ul className="justify-center flex gap-6 mx-auto text-sm">
+              <li><a className="underline" target="_blank" href="https://discuss.ipfs.tech/tos">Terms of Use</a></li>
+              <li><a className="underline" target="_blank" href="https://discuss.ipfs.tech/privacy">Privacy Policy</a></li>
+              <li><a className="underline" target="_blank" href="https://ipfs.tech/legal/">DMCA Policy</a></li>
+            </ul>
+            <p className="mx-auto text-center text-sm">Made with love by <a className="underline" href="https://protocol.ai/" target="_blank">Protocol Labs</a></p>
+          </div>
         </div>
       </ThemeContext.Provider>
     </>

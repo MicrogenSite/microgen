69caf9a035cb37f9bfd6e6305e7d6978a7588e2b
Tom Benbow <tom@hellomoon.net>
2023-05-09 10:59:20 -0500

    Fix nested headline error in accordian

--- components/blocks/accordian.tsx
+++ components/blocks/accordian.tsx
@@ -37,11 +37,9 @@ const AccordianItem = ({ data, index, cardstyle, isLast, parentField = "" }) => 
   return (
     <div className={`${borderWidthClass} first:border-t-0 last:border-b-0 ${borderColor(cardstyle?.borderStyles)} ${cardstyle?.fillStyles}`}>
       <div className={`relative cursor-pointer text-primary ${cardstyle?.padding} ${cardstyle?.headlineStyles} ${active && cardstyle?.fillStylesActive }`} onClick={() => setActive(!active)}>
-        <h4>
-          {data.headline && (
-            <h3 className={cardstyle?.headlineStyles} data-tinafield={`${parentField}.${index}.headline`}>{data.headline}</h3>
-          )}
-        </h4>
+        {data.headline && (
+          <h3 className={cardstyle?.headlineStyles} data-tinafield={`${parentField}.${index}.headline`}>{data.headline}</h3>
+        )}
         <div className={`absolute z-20 right-5 top-3.5 sm:top-4 ${active ? 'transform rotate-180' : ''}`}>
           <svg width="16" height="19" viewBox="0 0 16 19" className="fill-current" xmlns="http://www.w3.org/2000/svg">
             <path d="M8.92265 0.551269L7.5125 0.551269C7.04245 0.551269 6.78934 0.804374 6.78934 1.27442L6.78934 13.3873L2.48657 9.66303C2.16115 9.37377 1.76341 9.37377 1.47415 9.73535L0.64252 10.7839C0.353258 11.1455 0.389415 11.4709 0.750993 11.7963L7.5125 17.7624C7.7656 17.9793 7.91023 18.0878 8.1995 18.0878C8.56107 18.0878 8.63339 17.9793 8.8865 17.7624L15.6842 11.7963C16.0457 11.4709 16.0819 11.1455 15.7926 10.7839L14.961 9.73535C14.6717 9.33761 14.274 9.33761 13.9124 9.62687L9.64581 13.3511L9.64581 1.27442C9.64581 0.804374 9.3927 0.551269 8.92265 0.551269Z" />

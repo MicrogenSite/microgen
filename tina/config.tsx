fd08fafa15c401d278c6cc0caa692fd23b6a4000
Tom Benbow <tom@hellomoon.net>
2023-06-12 15:58:13 -0500

    Back to dynamic branch

--- tina/config.tsx
+++ tina/config.tsx
@@ -21,7 +21,10 @@ import { themeCollectionSchema } from "../schema/collections/themes"
 
 const config = defineStaticConfig({
   clientId: process.env.NEXT_PUBLIC_TINA_CLIENT_ID!,
-  branch: "v1.7", // This should be set to the current branch
+  branch:
+    process.env.NEXT_PUBLIC_TINA_BRANCH! || // custom branch env override
+    process.env.NEXT_PUBLIC_VERCEL_GIT_COMMIT_REF! || // Vercel branch env
+    process.env.HEAD!, // Netlify branch env
   token: process.env.TINA_TOKEN!,
   media: {
     // If you wanted cloudinary do this

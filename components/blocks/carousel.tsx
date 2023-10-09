b4134ec61b44f470bc5ad702a7474d180734193a
Tom Benbow <tom@hellomoon.net>
2023-10-09 15:54:25 -0500

    Fix padding bug that was offsetting arrows vertically

--- components/blocks/carousel.tsx
+++ components/blocks/carousel.tsx
@@ -13,7 +13,8 @@ export const Carousel = ({ data }) => {
   const [isPrevVisible, setIsPrevVisible] = useState(false);
   const [isNextVisible, setIsNextVisible] = useState(false);
   const style = data.style || {}
-  const padding = style.padding
+  const paddingY = style.padding.split(' ').filter(word => word.includes('pt') || word.includes('pb')).join(' ');
+  const paddingX = style.padding.split(' ').filter(word => word.includes('pl') || word.includes('pr')).join(' ');
   const width = style.fullWidth ? "" : "max-w-desktop-full mx-auto"
   const gap = (data.items?.length > 0 || style.loop) ? `space-x-${style.gap} px-${style.gap}` : ""
   
@@ -49,31 +50,33 @@ export const Carousel = ({ data }) => {
       background={data.background}
       navigationLabel={data.navigationLabel}
     >
-      <div className={`relative ${width} ${padding} ${style.minHeight}`}>
-        <div className={`embla ${style.overflowHidden && "overflow-hidden"}`} ref={emblaRef}>
-          <div className={`embla__container flex ${gap}`}>
-            {data.items &&
-              data.items.map(function (block, index) {
-                return <div className={`embla__slide min-w-0 flex-grow-0 flex-shrink-0 ${style.slides}`} key={index}>
-                  <Card data={block} cardstyle={{ ...data.cardStyle, equalHeights: style.equalHeights }} />
-                </div>;
-              })
-            }
+      <div className={paddingY}>
+        <div className={`relative ${width} ${paddingX} ${style.minHeight}`}>
+          <div className={`embla ${style.overflowHidden && "overflow-hidden"}`} ref={emblaRef}>
+            <div className={`embla__container flex ${gap}`}>
+              {data.items &&
+                data.items.map(function (block, index) {
+                  return <div className={`embla__slide min-w-0 flex-grow-0 flex-shrink-0 ${style.slides}`} key={index}>
+                    <Card data={block} cardstyle={{ ...data.cardStyle, equalHeights: style.equalHeights }} />
+                  </div>;
+                })
+              }
+            </div>
+            {style.showArrows && (
+              <>
+                {isPrevVisible && (
+                  <a className={`absolute top-1/2 transform -translate-y-1/2 left-${style.arrowInset || '0'} btn-${style.arrowButtonStyle || 'primary'}`} onClick={scrollPrev}>
+                    <FaIcon icon="chevron-left-solid" />
+                  </a>
+                )}
+                {isNextVisible && (
+                  <a className={`absolute top-1/2 transform -translate-y-1/2 right-${style.arrowInset || '0'} btn-${style.arrowButtonStyle || 'primary'}`} onClick={scrollNext}>
+                    <FaIcon icon="chevron-right-solid" />
+                  </a>
+                )}
+              </>
+            )}
           </div>
-          {style.showArrows && (
-            <>
-              {isPrevVisible && (
-                <a className={`absolute top-1/2 transform -translate-y-1/2 left-${style.arrowInset || '0'} btn-${style.arrowButtonStyle || 'primary'}`} onClick={scrollPrev}>
-                  <FaIcon icon="chevron-left-solid" />
-                </a>
-              )}
-              {isNextVisible && (
-                <a className={`absolute top-1/2 transform -translate-y-1/2 right-${style.arrowInset || '0'} btn-${style.arrowButtonStyle || 'primary'}`} onClick={scrollNext}>
-                  <FaIcon icon="chevron-right-solid" />
-                </a>
-              )}
-            </>
-          )}
         </div>
       </div>
     </Section>

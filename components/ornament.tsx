1dfb6371e6badeaef70ca21695fff38723f7549e
Tom Benbow <tom@hellomoon.net>
2023-12-21 15:01:57 -0600

    Fix ornament position

--- components/ornament.tsx
+++ components/ornament.tsx
@@ -4,25 +4,25 @@ import { Tween, ScrollTrigger } from 'react-gsap';
 const anchorPosition = {
   "center": "top-1/2 left-1/2",
   "top": "top-0 left-1/2",
-  "top-right": "top-0 right-0",
+  "right-top": "top-0 right-0",
   "right": "top-1/2 right-0",
-  "bottom-right": "bottom-0 right-0",
+  "right-bottom": "bottom-0 right-0",
   "bottom": "bottom-0 left-1/2",
-  "bottom-left": "left-0 bottom-0",
+  "left-bottom": "left-0 bottom-0",
   "left": "top-1/2 left-0",
-  "top-left": "top-0 left-0",
+  "left-top": "top-0 left-0",
 };
 
 const alignmentTransform = {
   "center": "-50%,-50%",
   "top": "-50%,0",
-  "top-right": "-100%,0",
+  "right-top": "-100%,0",
   "right": "-100%,-50%",
-  "bottom-right": "-100%,-100%",
+  "right-bottom": "-100%,-100%",
   "bottom": "-50%,-100%",
-  "bottom-left": "0,-100%",
+  "left-bottom": "0,-100%",
   "left": "0,-50%",
-  "top-left": "0,0"
+  "left-top": "0,0"
 };
 
 const getValue = (string, searchString) => {
